0b60e6de66565f446d5225df82d06e6e4116eab0
Valeriy Semenenko <gearmobile@gmail.com>
2024-10-19 21:45:53 +0300

    add mysql

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,21 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
-  imports: [],
+  imports: [
+    TypeOrmModule.forRoot({
+      type: 'mysql',
+      host: 'nestjs_mysql_db',
+      port: 3306,
+      username: 'nestjs_user',
+      password: 'nestjs_password',
+      database: 'nestjs_mysql_db',
+      entities: [],
+      synchronize: true,
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
